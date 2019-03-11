37ac0ff39bd2775203dabf929881b44e3fcb462d
Willard25 <47044174+Willard25@users.noreply.github.com>
2019-03-10 22:21:39 -0400

    adding files

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    public float speed;
+    public float jumpForce;
+    public Text countText;
+    public Text scoreText;
+    public Text livesText;
+    public Text winText;
+    public Text loseText;
+    public GameObject player;
+
+    private int count;
+    private int score;
+    private int lives;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        score = 0;
+        lives = 3;
+        SetCountText();
+        SetScoreText();
+        SetLivesText();
+        winText.text = "";
+        loseText.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+            Application.Quit();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector2 movement = new Vector2(moveHorizontal, 0);
+        rb2d.AddForce(movement * speed);
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "Ground")
+        {
+            if(Input.GetKey(KeyCode.UpArrow))
+            {
+                rb2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Pick Up")
+        {
+            collision.gameObject.SetActive(false);
+            count = count + 1;
+            score = score + 1;
+            SetCountText();
+            SetScoreText();
+        }
+    }
+
+    void SetScoreText()
+    {
+        scoreText.text = "Score: " + score.ToString();
+        if (score >= 1)
+        {
+            winText.text = "You Win";
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+    }
+
+    void SetLivesText()
+    {
+        livesText.text = "Lives: " + lives.ToString();
+        if (lives == 0)
+        {
+            loseText.text = "You Lose";
+            player.SetActive(false);
+        }
+    }
+}
